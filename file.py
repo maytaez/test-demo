f9515f6bf373f0c9b5c70bda393b823fe39e012e
Satyam <satyam.pant@stud.uni-bamberg.de>
2023-05-11 23:48:15 +0200

    Removed Code

--- file.py
+++ file.py
@@ -1,17 +1,4 @@
-import sys
-
 import requests
 
-print(sys.version)
-print(sys.executable)
-
-
-def greet(who_to_greet):
-    greeting = f"Hello,{who_to_greet}"
-    return greeting
-
-
-# print(greet("World"))
-# print(greet("Corey"))
 r = requests.get("http://google.com/")
 print(r.status_code)
